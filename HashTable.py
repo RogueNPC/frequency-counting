be4d032a8a3946bff0e521cc7233da03c2d1f84d
RogueNPC <jordanbtansay@gmail.com>
2021-02-25 19:57:10 -0800

    [ADD] created array method, hash_function method, and insert method

--- HashTable.py
+++ HashTable.py
@@ -6,35 +6,49 @@ class HashTable:
     self.size = size
     self.arr = self.create_arr(size)
 
-
-  # 1️⃣ TODO: Complete the create_arr method.
-
   # Each element of the hash table (arr) is a linked list.
   # This method creates an array (list) of a given size and populates each of its elements with a LinkedList object.
 
   def create_arr(self, size):
      
-    pass
+    temp = []
+    for i in range(size):
+      temp.append(LinkedList())
+    return temp
 
-
-
-
-  # 2️⃣ TODO: Create your own hash function.
 
   # Hash functions are a function that turns each of these keys into an index value that we can use to decide where in our list each key:value pair should be stored. 
 
   def hash_func(self, key):
     
-    pass
+    key_length = len(key)
+
+    index = key_length % self.size
+
+    return index
 
 
-  # 3️⃣ TODO: Complete the insert method.
-
-  # Should insert a key value pair into the hash table, where the key is the word and the value is a counter for the number of times the word appeared. When inserting a new word in the hash table, be sure to check if there is a Node with the same key in the table already.
+  # Inserts a key value pair into the hash table, where the key is the word and the value is a counter for the number of times the word appeared.
+  # When inserting a new word in the hash table, be sure to check if there is a Node with the same key in the table already.
 
   def insert(self, key, value):
-    pass
 
+    # First find the index where it should be placed
+    key_hash = self.hash_func(key)
+
+    # Access the linked list in the array
+    ll = self.arr[key_hash]
+
+    # Check if the index exists in the linked list
+    index_in_linkedlist = ll.find(key)
+      
+    # if not item not in linkedlist
+    if index_in_linkedlist == -1:
+      tup = (key, value)    # Creates tuple for new word
+      ll.append(tup)    # Append it to the linked list
+    # if item found in linkedlist
+    else:
+      ll.modify(index_in_linkedlist)
 
 
 
